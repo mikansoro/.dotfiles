ea345f0f3a05b24c116bcdfcc0178dee47b3f66a
Michael Rowland <michael@mikansystems.com>
2022-10-13 22:01:26 -0700

    start templating flake.nix

--- flake.nix
+++ flake.nix
@@ -2,9 +2,13 @@
   description = "timidtogekiss system config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-21.05";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-21.05";
+    # nixpkgs.url = "nixpkgs/nixos-21.05";
+    # nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+
+    nixpkgs.url = "github:nixos/nixpkgs";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-doom-emacs.url = "github:vlaci/nix-doom-emacs";
   };
@@ -12,26 +16,58 @@
   outputs = inputs@{ 
       nixpkgs,
       home-manager,
-      nixpkgs-unstable, 
+      # nixpkgs-unstable,
       nix-doom-emacs, 
       ...
   }: 
   let
-    system = "x86_64-linux";
 
-    globalPkgsConfig = import ./nix/config/nixpkgs/config.nix;
-
-    overlay-unstable = final: prev: {
-      unstable = import nixpkgs-unstable {
-        inherit system;
-        config = globalPkgsConfig;
-      }; 
+    workMac = darwin.lib.darwinSystem {
+      system = "x86-64-darwin";
+      modules = [
+        ./macos.nix
+        home-manager.darwinModules.home-manager {
+          home-manager.users.mrowland = import ./home.nix
+        }
+      ];
+      specialArgs = { inherit nixpkgs; };
     };
+
+    fedoraLaptop = home-manager.lib.homeManagerConfiguration {
+      configuration = [];
+      system = "x86-64-linux";
+      homeDirectory = "/home/michael";
+      username = "michael"
+    }
+
+    darwinConfig = {
+      system = "x86-64-darwin";
+      username = "mrowland";
+      homeDirectory = "/Users/${username}";
+    };
+
+    linuxConfig = {
+      system = "x86-64-linux";
+      username = "michael";
+      homeDirectory = "/home/${username}";
+    };
+
+    # globalPkgsConfig = import ./nix/config/nixpkgs/config.nix;
+
+    # overlay-unstable = final: prev: {
+    #   unstable = import nixpkgs-unstable {
+    #     inherit system;
+    #     config = globalPkgsConfig;
+    #   };
+    # };
 
     pkgs = import nixpkgs {
       inherit system;
-      config = globalPkgsConfig;
-      overlays = [ overlay-unstable ];
+      config = {
+        allowUnfree = true;
+      };
+      #config = globalPkgsConfig;
+      # overlays = [ overlay-unstable ];
     };
 
   in {
@@ -40,7 +76,7 @@
         inherit system pkgs;
         username = "michael";
         homeDirectory = "/home/michael";
-        stateVersion = "21.05";
+        # stateVersion = "21.05";
         configuration = {
             imports = [
               nix-doom-emacs.hmModule
