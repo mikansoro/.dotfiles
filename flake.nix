bfb9a81d82c229f395071315446c7e522491308a
Michael Rowland <michael@mikansystems.com>
2022-10-28 01:10:25 -0700

    add nixos-generators for a xen vmdk

--- flake.nix
+++ flake.nix
@@ -1,21 +1,27 @@
 {
-  description = "timidtogekiss system config";
+  description = "mikansoro system config";
 
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
     nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.05";
     # darwin.url "github:lnl7/nix-darwin";
     # darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
   };
 
   outputs = inputs@{
       nixpkgs,
       home-manager,
-      # nixpkgs-unstable,
+      nixpkgs-stable,
       nix-doom-emacs,
+      nixos-generators,
       ...
   }:
     let
@@ -26,7 +32,14 @@
           allowUnfree = true;
         };
       };
+      pkgs-stable = import nixpkgs-stable {
+        system = "x86_64-linux";
+        config = {
+          allowUnfree = true;
+        };
+      };
       overlays = [
+        # TODO: use the file instead of defining inline
         (self: super: {
           # found here: https://github.com/nubank/nixpkgs/blob/master/default.nix
           # cant just call overrideAttrs and replace version, since version is hard-coded in mkTerraform in nixpkgs
@@ -49,6 +62,33 @@
               nixpkgs.overlays = overlays;
             }
           ];
+        };
+      };
+      packages.x86_64-linux = {
+        # generate a VMDK for import to xenorchestra, not vcenter
+        # disables vmware guest settings, enables systemd uefi boot for xen, and xe guest utils for monitoring
+        xenVmdk = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          pkgs = pkgs-stable;
+          modules = [
+            "${inputs.nixpkgs-stable}/nixos/modules/virtualisation/xen-domU.nix"
+            ({ lib, ...} : {
+              services.xe-guest-utilities.enable = true;
+              services.openssh.enable = true;
+              # TODO: setup a bootstrap user w/ ssh key
+              boot.loader.systemd-boot.enable = true;
+              boot.loader.efi.canTouchEfiVariables = true;
+              virtualisation.vmware.guest.enable = lib.mkForce false;
+              system.stateVersion = "22.05";
+            })
+            # create a separate module to not have to wrap the options above in config = {}
+            # best way? probably not. /shrug
+            ({ config, ...} : {
+              # same derivation name as upstream vmware-image, but replace vmware with xen
+              vmware.vmDerivationName = "nixos-xen-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}";
+            })
+          ];
+          format = "vmware";
         };
       };
     };
