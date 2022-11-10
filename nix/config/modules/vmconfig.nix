066d7b8e49f79c0438e211906f17797595f3e9de
Michael Rowland <michael@mikansystems.com>
2022-11-09 22:40:27 -0800

    add initial password for vm image users

--- nix/config/modules/vmconfig.nix
+++ nix/config/modules/vmconfig.nix
@@ -7,6 +7,7 @@
     createHome = true;
     group = "nixos";
     isNormalUser = true;
+    initialPassword = "nixos";
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDPCKkMo4zEB0IVy4ojrLV2RPGl+1MBXoNuK2vZjMdJT mikans@localhost"
