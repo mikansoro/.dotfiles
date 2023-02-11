b9834e20a2fdd962caf4fb7e8412a8e84ee30da8
Michael Rowland <michael@mikansystems.com>
2023-02-11 03:14:52 -0800

    update glaceon boot v2

--- nix/hosts/glaceon/configuration.nix
+++ nix/hosts/glaceon/configuration.nix
@@ -12,7 +12,7 @@
 
   # Use the systemd-boot EFI boot loader.
   #boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.canTouchEfiVariables = false;
   boot.loader.grub = {
     enable = true;
     version = 2;
@@ -20,11 +20,12 @@
     efiSupport = true;
     enableCryptodisk = true;
     useOSProber = true;
+    efiInstallAsRemovable = true;
   };
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot/EFI";
   boot.initrd.luks.devices = {
     root = {
-      device = "";
+      device = "/dev/disk/by-uuid/dd30ec7a-19a5-4e5a-ba02-6dd8bccd5360"; # /dev/nvme0n1p2
       preLVM = true;
     };
   };
