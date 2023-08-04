82e73360ea558db3e102f37c4e260dd00f381282
Michael Rowland <michael@mikansystems.com>
2023-08-04 01:56:57 -0700

    enable gpg-agent for x220

--- nix/hosts/x220/home.nix
+++ nix/hosts/x220/home.nix
@@ -6,6 +6,7 @@
     ../../home-modules/shell
     ../../home-modules/git
     ../../home-modules/emacs
+    ../../home-modules/gpg-agent
 
     # BUG: The option `home-manager.users.michael.home.file."/home/michael/.config/gnupg/gpg-agent.conf".text` is defined both null and not null, in `/mnt/nix/store/6zxkqbrcw5r5yi6hmk3xf17y6wqiqs0h-source/modules/misc/xdg.nix' and `/mnt/nix/store/6zxkqbrcw5r5yi6hmk3xf17y6wqiqs0h-source/modules/services/gpg-agent.nix'.
     #../../home-modules/gpg-agent
