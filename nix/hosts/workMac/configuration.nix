15e11ca642278ec77582c984ef8eb913683dee98
Michael Rowland <michael@mikansystems.com>
2024-07-29 15:12:00 -0700

    fix emacs path on macos

--- nix/hosts/workMac/configuration.nix
+++ nix/hosts/workMac/configuration.nix
@@ -44,6 +44,14 @@
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
 
+  # workaround for a single user system: https://github.com/LnL7/nix-darwin/pull/252#issuecomment-731083818
+  # emacs doesn't handle $USER or $HOME expansion in PATH, so even when the path is set via launchd packages
+  # installed via homeassistant aren't found. Have to replace the variables to be static before the PATH is
+  # set, otherwise emacs won't load additional packages from path (ie ripgrep, language servers)
+  launchd.user.envVariables.PATH = (lib.replaceStrings ["$HOME" "$USER"]
+    [config.users.users."michael.rowland".home config.users.users."michael.rowland".name]
+    config.environment.systemPath);
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
