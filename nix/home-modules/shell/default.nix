cdcc8ed5b81fdee4fd91efaffc52f694b929efea
Michael Rowland <michael@mikansystems.com>
2024-10-14 11:18:04 -0700

    enable wezterm

--- nix/home-modules/shell/default.nix
+++ nix/home-modules/shell/default.nix
@@ -1,8 +1,17 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   imports = [
     ./clitools.nix
     ./zsh.nix
   ];
+
+  programs.wezterm = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  xdg.configFile."wezterm/wezterm.lua".text = lib.mkForce null;
+  xdg.configFile."wezterm/wezterm.lua".source = ./wezterm/wezterm.lua;
 }
---
+++ nix/home-modules/shell/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'ayu'
+config.font = wezterm.font 'Source Code Pro'
+config.front_end = "WebGpu"
+
+return config
