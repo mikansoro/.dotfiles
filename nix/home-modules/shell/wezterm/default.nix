05e30ca05aff03cce005f9df515f8a0f73e40799
Michael Rowland <michael@mikansystems.com>
2024-11-18 11:09:25 -0800

    remove wezterm binary cache config for now

--- nix/home-modules/shell/wezterm/default.nix
+++ nix/home-modules/shell/wezterm/default.nix
@@ -11,8 +11,8 @@
   xdg.configFile."wezterm/wezterm.lua".text = lib.mkForce null;
   xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
 
-  nix.settings = {
-    substituters = ["https://cache.nixos.org" "https://wezterm.cachix.org"];
-    trusted-public-keys = ["wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="];
-  };
+  # nix.settings = {
+  #   substituters = ["https://cache.nixos.org" "https://wezterm.cachix.org"];
+  #   trusted-public-keys = ["wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="];
+  # };
 }
