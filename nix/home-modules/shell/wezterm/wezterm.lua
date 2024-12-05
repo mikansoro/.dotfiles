741f6288dc804574ad2ed1d64bd1c21edff5472f
Michael Rowland <michael@mikansystems.com>
2024-12-04 23:30:33 -0800

    more wezterm config updates

--- nix/home-modules/shell/wezterm/wezterm.lua
+++ nix/home-modules/shell/wezterm/wezterm.lua
@@ -5,7 +5,13 @@ local config = {
 
   color_scheme = 'ayu',
   font = wezterm.font('Source Code Pro'),
+  font_size = 10.0,
   front_end = "WebGpu",
+
+  initial_cols = 120,
+  initial_rows = 30,
+
+  hide_tab_bar_if_only_one_tab = true,
 
   enable_wayland = true,
   --dpi = 72,
