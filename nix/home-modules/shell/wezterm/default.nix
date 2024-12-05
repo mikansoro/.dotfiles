12908fbfa5ad9567c5e5d5e6365400255a8b1cb2
Michael Rowland <michael@mikansystems.com>
2024-12-05 14:54:20 -0800

    update wezterm conf to have conditional sizing

--- nix/home-modules/shell/wezterm/default.nix
+++ nix/home-modules/shell/wezterm/default.nix
@@ -6,10 +6,26 @@
     package = wezterm.packages.${pkgs.system}.default;
     enableBashIntegration = true;
     enableZshIntegration = true;
-  };
+    extraConfig = ''
+    local config = {
+      audible_bell = "Disabled",
 
-  xdg.configFile."wezterm/wezterm.lua".text = lib.mkForce null;
-  xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
+      color_scheme = 'ayu',
+      font = wezterm.font('Source Code Pro'),
+      font_size = ${ if pkgs.stdenv.isDarwin then "13.0" else "10.0" },
+      front_end = "WebGpu",
+
+      initial_cols = 120,
+      initial_rows = 30,
+
+      hide_tab_bar_if_only_one_tab = true,
+
+      enable_wayland = true,
+    }
+
+    return config
+    '';
+  };
 
   # nix.settings = {
   #   substituters = ["https://cache.nixos.org" "https://wezterm.cachix.org"];
