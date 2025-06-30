b8a85dc19de6ccde7992263aa7ce113150dd1cab
Michael Rowland <michael@mikansystems.com>
2025-06-30 10:35:51 -0700

    update flale darwin ref

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     darwin = {
-      url = "github:lnl7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
