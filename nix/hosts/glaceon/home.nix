ec000a8799c3c70cf900b83ee906a1133fe868df
Michael Rowland <michael@mikansystems.com>
2026-04-23 04:50:41 -0700

    fix: improper claude hm module

--- nix/hosts/glaceon/home.nix
+++ nix/hosts/glaceon/home.nix
@@ -7,6 +7,7 @@
     ../../home-modules/git
     ../../home-modules/emacs
     ../../home-modules/firefox
+    ../../home-modules/claude
 
     # BUG: The option `home-manager.users.michael.home.file."/home/michael/.config/gnupg/gpg-agent.conf".text` is defined both null and not null, in `/mnt/nix/store/6zxkqbrcw5r5yi6hmk3xf17y6wqiqs0h-source/modules/misc/xdg.nix' and `/mnt/nix/store/6zxkqbrcw5r5yi6hmk3xf17y6wqiqs0h-source/modules/services/gpg-agent.nix'.
     #../../home-modules/gpg-agent
