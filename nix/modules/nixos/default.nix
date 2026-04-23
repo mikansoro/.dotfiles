0da07e88689a861d6439c17c2f032e33ae75b717
Michael Rowland <michael@mikansystems.com>
2026-04-23 05:30:12 -0700

    conf: add some default nixos settings

--- nix/modules/nixos/default.nix
+++ nix/modules/nixos/default.nix
@@ -1,5 +1,8 @@
-{ mLib, ... }:
+{ lib, mLib, ... }:
 
 {
   imports = mLib.importSubmodules ./.;
+
+  time.timeZone = lib.mkDefault "America/Los_Angeles";
+  networking.domain = lib.mkDefault "int.mikansystems.com";
 }
